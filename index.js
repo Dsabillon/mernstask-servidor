51f88512ce8b3592d53768a3f07724453a525621
DavidS <dsabillon.94@gmail.com>
2020-04-30 22:01:54 -0600

    ajustes para deployment

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ app.use(cors());
 
 app.use(express.json({ extended: true }));
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 //Routes
 app.use("/api/usuarios", require("./routes/usuarios"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/proyectos", require("./routes/proyectos"));
 app.use("/api/tareas", require("./routes/tareas"));
-app.listen(PORT, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(
-    `El servidor esta funcionando en el puerto ${PORT}`.underline.yellow
+    `El servidor esta funcionando en el puerto ${port}`.underline.yellow
   );
 });
